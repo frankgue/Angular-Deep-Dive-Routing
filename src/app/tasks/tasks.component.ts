549c1e81daea50924d0dba410c01277708bb19bd
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2026-01-06 14:05:21 +0100

    Extracting Query Parameters via input

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,31 +1,31 @@
-import { Component, computed, effect, inject, input, OnInit } from '@angular/core';
+import { Component, computed, inject, input, OnInit } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
-import { Task } from './task/task.model';
 import { TasksService } from './tasks.service';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
-  imports: [TaskComponent],
+  imports: [TaskComponent, RouterLink],
 })
 export class TasksComponent implements OnInit {
   userId = input.required<string>();
+  order = input<'asc' | 'desc'>('asc');
   userTasks = computed(() => this.tasksService.allTasks().filter(task => task.userId === this.userId()));
 
 
   private tasksService = inject(TasksService);
 
   constructor(){
-    // effect(() => {
-    //     this.userTasks = this.tasksService.allTasks().filter(task => task.userId = this.userId())
-    // })
+ 
   }
 
   ngOnInit(): void {
-      // this.userTasks = this.tasksService.allTasks().filter(task => task.userId === this.userId())
+    console.log(this.order);
+    
   }
 
 }
