fc3eb312ce1e5a3aee7c931809fd392d52392fec
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2026-01-06 11:14:47 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "./header/header.component";
+import { UsersComponent } from "./users/users.component";
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, UsersComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -0,0 +1,16 @@
+import { Component, computed, inject, input } from '@angular/core';
+import { UsersService } from '../users.service';
+
+@Component({
+  selector: 'app-user-tasks',
+  standalone: true,
+  templateUrl: './user-tasks.component.html',
+  styleUrl: './user-tasks.component.css',
+})
+export class UserTasksComponent {
+  userId = input.required<string>();
+
+  private userService = inject(UsersService);
+
+  userName = computed(() => this.userService.users.find(user => user.id === this.userId())?.name);
+}
