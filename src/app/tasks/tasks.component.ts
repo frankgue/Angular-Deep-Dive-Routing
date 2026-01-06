dc6b89c9fbbaf8f9fd3be99558fb10cafbaa0d2c
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2026-01-06 12:50:38 +0100

    extract dynamic route parameter via withComponentInputBinding() and Observable ActivatedRoute and Router

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, inject, input, OnInit } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { Task } from './task/task.model';
+import { TasksService } from './tasks.service';
 
 @Component({
   selector: 'app-tasks',
@@ -10,6 +11,21 @@ import { Task } from './task/task.model';
   styleUrl: './tasks.component.css',
   imports: [TaskComponent],
 })
-export class TasksComponent {
-  userTasks: Task[] = [];
+export class TasksComponent implements OnInit {
+  userId = input.required<string>();
+  userTasks = computed(() => this.tasksService.allTasks().filter(task => task.userId === this.userId()));
+
+
+  private tasksService = inject(TasksService);
+
+  constructor(){
+    // effect(() => {
+    //     this.userTasks = this.tasksService.allTasks().filter(task => task.userId = this.userId())
+    // })
+  }
+
+  ngOnInit(): void {
+      // this.userTasks = this.tasksService.allTasks().filter(task => task.userId === this.userId())
+  }
+
 }
--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,18 +1,39 @@
-import { Component, computed, inject, input } from '@angular/core';
+import { Component, computed, inject, input, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
+import { ActivatedRoute, RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-tasks',
   standalone: true,
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
+  imports: [RouterOutlet, RouterLinkWithHref],
 })
-export class UserTasksComponent {
-  userId = input.required<string>();
-
+export class UserTasksComponent implements OnInit {
+  // userId = input.required<string>();
   private userService = inject(UsersService);
+  private activatedRoute = inject(ActivatedRoute);
+  userName: string = '';
+  subscription?: Subscription;
 
-  userName = computed(
-    () => this.userService.users.find((user) => user.id === this.userId())?.name
-  );
+  // userName = computed(
+  //   () => this.userService.users.find((user) => user.id === this.userId())?.name
+  // );
+
+  ngOnInit(): void {
+    console.log(this.activatedRoute);
+    this.subscription = this.activatedRoute.paramMap.subscribe({
+      next: (paramMap) => {
+        this.userName =
+          this.userService.users.find(
+            (user) => user.id === paramMap.get('userId')
+          )?.name || '';
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 }
