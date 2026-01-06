399b446130e6a05bbbdf15794f57273f16516475
Frank GUEKENG - GKFC <frankguekeng11@gmail.com>
2026-01-06 15:50:00 +0100

    Resolving Route-related Dynamic Data via Resolver

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,16 @@
-import { Component, computed, inject, input, OnInit } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  input,
+  OnInit,
+  signal,
+} from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { TasksService } from './tasks.service';
-import { RouterLink } from "@angular/router";
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-tasks',
@@ -13,19 +21,36 @@ import { RouterLink } from "@angular/router";
 })
 export class TasksComponent implements OnInit {
   userId = input.required<string>();
-  order = input<'asc' | 'desc'>('asc');
-  userTasks = computed(() => this.tasksService.allTasks().filter(task => task.userId === this.userId()));
-
+  // order = input<'asc' | 'desc'>('asc');
+  // order: 'asc' | 'desc' = 'asc';
+  order = signal<'asc' | 'desc'>('desc');
+  userTasks = computed(() =>
+    this.tasksService.allTasks().filter((task) => task.userId === this.userId()).sort((a, b) => {
+      if (this.order() === 'desc') {
+        return a.id > b.id ? -1 : 1
+      } else {
+        return a.id > b.id ? 1 : -1
+      }
+    })
+  );
 
   private tasksService = inject(TasksService);
+  private activatedRoute = inject(ActivatedRoute);
+  subscription?: Subscription;
 
-  constructor(){
- 
-  }
+  constructor() {}
 
   ngOnInit(): void {
     console.log(this.order);
-    
+    this.subscription = this.activatedRoute.queryParams.subscribe({
+      next: (queryParams) => {
+        console.log(queryParams);
+        this.order.set(queryParams['order']);
+      },
+    });
   }
 
+  ngOnDestroy(): void {
+    this.subscription?.unsubscribe();
+  }
 }
--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,6 +1,12 @@
-import { Component, computed, inject, input, OnInit } from '@angular/core';
+import { Component, inject, input, OnInit } from '@angular/core';
 import { UsersService } from '../users.service';
-import { ActivatedRoute, RouterOutlet, RouterLinkWithHref } from '@angular/router';
+import {
+  RouterOutlet,
+  RouterLinkWithHref,
+  ResolveFn,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+} from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -11,32 +17,28 @@ import { Subscription } from 'rxjs';
   imports: [RouterOutlet, RouterLinkWithHref],
 })
 export class UserTasksComponent implements OnInit {
-  // userId = input.required<string>();
-  private userService = inject(UsersService);
-  private activatedRoute = inject(ActivatedRoute);
-  userName: string = '';
+  message = input.required<string>();
+  userName = input.required<string>();
   subscription?: Subscription;
 
-  // userName = computed(
-  //   () => this.userService.users.find((user) => user.id === this.userId())?.name
-  // );
-
   ngOnInit(): void {
-    console.log(this.activatedRoute);
-    console.log(this.activatedRoute.snapshot);
-    console.log(this.activatedRoute.snapshot.paramMap.get('userId'));
-    // this.subscription = this.activatedRoute.snapshot.paramMap.get('userId')
-    this.subscription = this.activatedRoute.paramMap.subscribe({
-      next: (paramMap) => {
-        this.userName =
-          this.userService.users.find(
-            (user) => user.id === paramMap.get('userId')
-          )?.name || '';
-      },
-    });
+      
   }
 
-  ngOnDestroy(): void {
-    this.subscription?.unsubscribe();
-  }
+  // ngOnDestroy(): void {
+  //   this.subscription?.unsubscribe();
+  // }
 }
+
+export const resolveUserName: ResolveFn<string> = (
+  activatedRouteSnapshot: ActivatedRouteSnapshot,
+  routerState: RouterStateSnapshot
+) => {
+  const userService = inject(UsersService);
+  const userName =
+    userService.users.find(
+      (user) => user.id === activatedRouteSnapshot.paramMap.get('userId')
+    )?.name || '';
+
+  return userName;
+};
